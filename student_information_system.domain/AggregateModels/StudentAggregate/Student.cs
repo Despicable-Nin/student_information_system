598c8cb615c4f85b591af42aab9b653d8ad8d67e
Ninism <nin.alamo@outlook.com>
2022-11-05 21:44:19 +0800

    Update

--- student_information_system.domain/AggregateModels/StudentAggregate/Student.cs
+++ student_information_system.domain/AggregateModels/StudentAggregate/Student.cs
@@ -4,12 +4,13 @@ namespace student_information_system.domain.AggregateModels.StudentAggregate;
 
 internal sealed class Student : Entity, IAggregateRoot
 {
-    public Student(string lrn, string lastname, string firstname, string middlename, Gender gender, Address address)
+    public Student(string lrn, string lastname, string firstname, string middlename, DateTimeOffset dob, Gender gender, Address address)
     {
         LRN = lrn;
         LastName = lastname;
         FirstName = firstname;
         MiddleName = middlename;
+        DoB = dob;
         Gender = gender;
         Address = address;
     }
@@ -18,40 +19,9 @@ internal sealed class Student : Entity, IAggregateRoot
     public string LastName { get; }
     public string FirstName { get; }
     public string MiddleName { get; }
+    public DateTimeOffset DoB { get; }
 
     public Gender Gender { get; }
 
     public Address Address { get; }
-}
-
-public class Address : ValueObject
-{
-    private Address()
-    {
-    }
-
-    public Address(string street, string city, string state, string country, string zipcode)
-    {
-        Street = street;
-        City = city;
-        State = state;
-        Country = country;
-        ZipCode = zipcode;
-    }
-
-    public string Street { get; }
-    public string City { get; }
-    public string State { get; }
-    public string Country { get; }
-    public string ZipCode { get; }
-
-    protected override IEnumerable<object> GetEqualityComponents()
-    {
-        // Using a yield return statement to return each element one at a time
-        yield return Street;
-        yield return City;
-        yield return State;
-        yield return Country;
-        yield return ZipCode;
-    }
 }
