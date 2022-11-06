ca905a5670b77a19f49aadd20e7e5172273bbafd
Ninism <nin.alamo@outlook.com>
2022-11-06 08:31:43 +0800

    Removed unnecessary

---
+++ student_information.app/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace student_information.app.Models;
+
+public class ErrorViewModel
+{
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
